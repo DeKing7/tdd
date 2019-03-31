ea7f54c2545db24cfb85c54c2ec73627463a4028
Damian Kardela <DamKar@gmail.com>
2019-03-31 14:25:12 +0200

    niedziela 5 zadanie

--- src/main/java/project/Project.java
+++ src/main/java/project/Project.java
@@ -1,4 +1,11 @@
 package project;
 
 public class Project {
+    ProjectService service;
+
+    double timeofdrivingKubica = 1.3179;
+    int scoreofhalf = 1
+    int FederVictoria = 2;
+    int numbersof_Basketballteam = 5;
+
 }
---
+++ src/main/java/project/ProjectService.java
@@ -0,0 +1,9 @@
+package project;
+
+public interface ProjectService {
+
+    double howmanytimedriveKubica = (double okrazenie, double czas);
+    int howmanygoalsscoreRealinhalf = (int polowa, int wynik);
+    int howmanyvictoryhaveFederer = (int zawody, int wygrane);
+    int howmanytimequarter = (int punkty, int time);
+}
