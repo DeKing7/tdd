9ae5bfdcfc5e9dfe46541da78302d2005d65f88e
Damian Kardela <DamKar@gmail.com>
2019-03-31 14:49:55 +0200

    niedziela 5 zadanie 2

--- src/main/java/project/Project.java
+++ src/main/java/project/Project.java
@@ -1,11 +1,32 @@
 package project;
 
 public class Project {
+
     ProjectService service;
 
-    double timeofdrivingKubica = 1.3179;
-    int scoreofhalf = 1
-    int FederVictoria = 2;
-    int numbersof_Basketballteam = 5;
+   public Project(ProjectService) {
+       this.service = service;
+   }
 
+    double czas = 9.3677;
+    int okrazenia = 8;
+    int wynik = 1;
+    int wynik2 = 0;
+    int zawody = 70;
+    int wygrane = 20;
+    int punkty = 30;
+    int time = 15;
+
+    public double jednookrazenie (double czas, int okrazenia){
+           return service.howmanytimedriveKubica(czas, okrazenia);
+              }
+              public int wynik (int wynik, int wynik2){
+        return service.howmanygoalsscoreRealinhalf(wynik, wynik2);
+              }
+    public int iloscwyganych (int zawody, int wygrane){
+    return service.howmanyvictoryhaveFederer(zawody, wygrane);
+    }
+    public int punktynaminuty (int punkty, int time){
+        return service.howmanytimegetinquarter(punkty, time);
+    }
 }
--- src/main/java/project/ProjectService.java
+++ src/main/java/project/ProjectService.java
@@ -2,8 +2,14 @@ package project;
 
 public interface ProjectService {
 
-    double howmanytimedriveKubica = (double okrazenie, double czas);
-    int howmanygoalsscoreRealinhalf = (int polowa, int wynik);
-    int howmanyvictoryhaveFederer = (int zawody, int wygrane);
-    int howmanytimequarter = (int punkty, int time);
+
+    double howmanytimedriveKubica(double okrazenia, double czas);
+
+    int howmanygoalsscoreRealinhalf(int polowa, int wynik);
+
+    int howmanyvictoryhaveFederer(int zawody, int wygrane);
+
+    int howmanytimegetinquarter(int punkty, int time);
+
+
 }
